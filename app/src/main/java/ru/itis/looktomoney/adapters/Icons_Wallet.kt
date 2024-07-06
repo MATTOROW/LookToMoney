440e6fe0041c1f73fe3ab429c01d0da54ee915f7
VlaADanilov <helloampro@gmail.com>
2024-07-06 10:41:45 +0300

    Фрагмент добавления категории теперь реально может добавить категорию

---
+++ app/src/main/java/ru/itis/looktomoney/adapters/Icons_Wallet.kt
@@ -0,0 +1,9 @@
+package ru.itis.looktomoney.adapters
+
+import ru.itis.looktomoney.R
+
+object Icons_Wallet {
+    val list = listOf(
+        R.drawable.baseline_wallet_24
+    )
+}
--- app/src/main/java/ru/itis/looktomoney/fragments/accounts/CreateNewAccountFragment.kt
+++ app/src/main/java/ru/itis/looktomoney/fragments/accounts/CreateNewAccountFragment.kt
@@ -3,8 +3,14 @@ package ru.itis.looktomoney.fragments.accounts
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import ru.itis.looktomoney.R
+import ru.itis.looktomoney.adapters.IconSpinnerAdapter
+import ru.itis.looktomoney.adapters.Icons_Wallet
 import ru.itis.looktomoney.databinding.FragmentCreateNewAccountBinding
+import ru.itis.looktomoney.domain.DB_wallet_Helper
+import ru.itis.looktomoney.domain.Wallet
 
 class CreateNewAccountFragment : Fragment(R.layout.fragment_create_new_account) {
 
@@ -13,5 +19,55 @@ class CreateNewAccountFragment : Fragment(R.layout.fragment_create_new_account) 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCreateNewAccountBinding.bind(view)
+
+        val db_wallet = DB_wallet_Helper(requireContext(), null)
+
+        binding?.run {
+
+            confirmationButton.setOnClickListener {
+                var text = inputAccName.text.toString()
+                var numb = -1
+                try{
+                    numb = inputAccSum.text.toString().toInt()
+                } catch (_ : Exception){}
+
+                spinnerIconWallet.adapter = IconSpinnerAdapter(requireContext(), Icons_Wallet.list)
+
+                var polz_icon = -1
+
+                val itemIconSelected: AdapterView.OnItemSelectedListener =
+                    object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(
+                            parent: AdapterView<*>,
+                            view: View,
+                            position: Int,
+                            id: Long
+                        ) {
+
+                            val item = parent.getItemAtPosition(position) as Int
+                            polz_icon = item
+                        }
+
+                        override fun onNothingSelected(parent: AdapterView<*>?) {
+                        }
+                    }
+
+                spinnerIconWallet.onItemSelectedListener = itemIconSelected
+
+                if (text == "" || numb == -1 || polz_icon == -1){
+                    Toast.makeText(requireContext(), "Ошибка", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    db_wallet.addWallet(Wallet(numb, text, polz_icon))
+                    Toast.makeText(requireContext(), "Успешно", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
