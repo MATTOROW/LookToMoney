f9b28acaf51f88ed89da54d5661a63dc39e755aa
VlaADanilov <helloampro@gmail.com>
2024-07-09 10:06:44 +0300

    Накидал иконки

--- app/src/main/java/ru/itis/looktomoney/adapters/Icons.kt
+++ app/src/main/java/ru/itis/looktomoney/adapters/Icons.kt
@@ -6,6 +6,18 @@ object Icons {
     val list : List<Int> = listOf(
         R.drawable.baseline_attach_money_24,
         R.drawable.baseline_money_24,
-        R.drawable.baseline_wallet_24
+        R.drawable.baseline_wallet_24,
+        R.drawable.icon_1,
+        R.drawable.icon_2,
+        R.drawable.icon_3,
+        R.drawable.icon_4,
+        R.drawable.icon_5,
+        R.drawable.icon_6,
+        R.drawable.icon_7,
+        R.drawable.icon_8,
+        R.drawable.icon_9,
+        R.drawable.icon_10,
+        R.drawable.icon_11,
+        R.drawable.icon_12
     )
 }
